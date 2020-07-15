698aead947b52a5b1b16e0914b5b355e114a6fcc
github-guishu <67036076+github-guishu@users.noreply.github.com>
2020-07-15 15:33:22 +0800

    对Test类做了修改

---
+++ springboot_demo1/src/main/java/com/example/springboot_demo1/test/Test.java
@@ -0,0 +1,20 @@
+package com.example.springboot_demo1.test;
+
+/**
+ * @ClassName Test
+ * @Author liuxiaobo
+ * @Date 2020/7/15&15:23
+ * @Version 1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+        System.out.println("hello git ");
+        System.out.println("hello git project");
+    }
+    public void test1(){
+        int i = 10;
+        int m = 5;
+        System.out.println(i+m);
+    }
+}
